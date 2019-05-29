a6f475c0dbba876f6f7df0349bffc564a688e02e
Ismael Muñoz Contreras <Ismael@Ismaels-MacBook-Air.local>
2019-05-29 14:09:13 -0500

    fixed high lows

--- Submarine.py
+++ Submarine.py
@@ -9,28 +9,45 @@ def init():
 	gpio.setup(13,gpio.OUT)
 	gpio.setup(19,gpio.OUT)
 	gpio.setup(26,gpio.OUT)
+	gpio.setup(9,gpio.OUT)
+	gpio.setup(11,gpio.IN)
 	
 
-def Forward(t):
+def Forward():
 	init()
 	gpio.output(6,gpio.LOW)
 	gpio.output(13,gpio.HIGH)
 	gpio.output(19,gpio.LOW)
 	gpio.output(26,gpio.HIGH)
-	time.sleep(t)
+	# time.sleep(t)
 	gpio.cleanup()
 
 
 
-def Backward(t):
+def Backward():
 	init()
 	gpio.output(6,gpio.HIGH)
 	gpio.output(13,gpio.LOW)
 	gpio.output(19,gpio.HIGH)
 	gpio.output(26,gpio.LOW)
-	time.sleep(t)
+	# time.sleep(t)
 	gpio.cleanup()
 
-Forward(5)
-Backward(5)
+def Detenerse():
+
+	gpio.output(6,gpio.LOW)
+	gpio.output(13,gpio.LOW)
+	gpio.output(19,gpio.LOW)
+	gpio.output(26,gpio.LOW)
+
+
+def Distancia():
+	return gpio.input(11)
+	
+
+if Distancia() < 10:
+	Detenerse()
+else:
+	Forward()
+
 
