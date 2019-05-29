0def864599dd003bf6361064a62f350314f99f93
Ismael Muñoz Contreras <Ismael@Ismaels-MacBook-Air.local>
2019-05-29 13:33:01 -0500

    se borraron los archibos antiguos y se añadio uno nuevo

--- Submarine.py
+++ Submarine.py
@@ -1,26 +1,33 @@
+import RPi.GPIO as gpio
 import time
-import math
-import IMU
-import datetime
-import os
-from LSM9DS0 import *
 
-import smbus
-bus = smbus.SMBus(1)
+def init():
+
+	gpio.setmode(gpio.BCM)
+
+	gpio.setup(6,gpio.OUT)
+	gpio.setup(13,gpio.OUT)
+	gpio.setup(19,gpio.OUT)
+	gpio.setup(26,gpio.OUT)
+	
+
+def Forward():
+	init()
+	gpio.output(6,gpio.LOW)
+	gpio.output(13,gpio.LOW)
+	gpio.output(19,gpio.HIGH)
+	gpio.output(26,gpio.HIGH)
+	gpio.cleanup()
 
 
 
-class Submarine():
-	def robotInit(self):
-		
-		bus.write_byte_data(ACC_ADDRESS,LSM9DS0_CTRL_REG1_XM, 0b01100111)  #z,y,x axis enabled, continuos update,  100Hz data rate
-		bus.write_byte_data(ACC_ADDRESS,LSM9DS0_CTRL_REG2_XM, 0b00100000)  #+/- 16G full scale
+def Backward():
+	init()
+	gpio.output(6,gpio.LOW)
+	gpio.output(13,gpio.LOW)
+	gpio.output(19,gpio.HIGH)
+	gpio.output(26,gpio.HIGH)
+	gpio.cleanup()
 
-		#Initialize the magnetometer
-		bus.write_byte_data(ACC_ADDRESS,LSM9DS0_CTRL_REG5_XM, 0b11110000)  #Temp enable, M data rate = 50Hz
-		bus.write_byte_data(ACC_ADDRESS,LSM9DS0_CTRL_REG6_XM, 0b01100000)  #+/-12gauss
-		bus.write_byte_data(ACC_ADDRESS,LSM9DS0_CTRL_REG7_XM, 0b00000000)  #Continuous-conversion mode
+Forward()
 
-		#initialize the gyroscope
-		bus.write_byte_data(ACC_ADDRESS,LSM9DS0_CTRL_REG1_G, 0b00001111)   #Normal power mode, all axes enabled
-		bus.write_byte_data(ACC_ADDRESS,LSM9DS0_CTRL_REG4_G, 0b00110000)   #Continuos update, 2000 dps full scale
